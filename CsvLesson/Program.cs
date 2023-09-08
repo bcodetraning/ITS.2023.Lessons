6c7725d697ddc15c7e78f4a183684b8278d4de45
bcodetraning <bcode.training@gmail.com>
2023-09-08 16:02:30 +0200

    bug fix

--- CsvLesson/Program.cs
+++ CsvLesson/Program.cs
@@ -19,16 +19,55 @@ namespace CsvLesson
             people.Add(new() { Name = "Marco", Age = 30 });
             people.Add(new() { Name = "Diego", Age = 20 });
             people.Add(new() { Name = "Mauro", Age = 10 });
-            Console.WriteLine("Hello World!");
+          
+
+
+
+            saveToFile(people,@"D:\logs\saveToFile.csv"); 
             var result = LoadFromTextFileCsvHelper<Person>(@"D:\logs\saveToFile.csv");
 
-            foreach (var item in result.ToList())
+            foreach (var p in result.ToList())
             {
-                //Console.Write($" Album: {item.Album}");
-                //Console.Write($" -  ");
-                //Console.Write($"Title: {item.Title}");
-                //Console.WriteLine($"  ");
+                Console.Write($" Album: {p.Name}");
+                Console.Write($" -  ");
+                Console.Write($"Title: {p.Age}");
+                Console.WriteLine($"  ");
             }
+        }
+        public static void saveToFile<T>(List<T> data, string filePath) where T : class
+        {
+            List<string> lines = new List<string>(); // 
+            StringBuilder line = new StringBuilder();
+
+            if (data == null || data.Count == 0)
+            {
+                throw new ArgumentException("data", "La lista è vuota!");
+            }
+
+            var cols = data[0].GetType().GetProperties();
+
+            foreach (var col in cols)// cicla tutte le Entity della classe in oggetto
+            {
+                line.Append(col.Name);
+                line.Append(",");
+            }
+
+            lines.Add(line.ToString().Substring(0, line.Length - 1));
+
+            foreach (var row in data)
+            {
+
+                line = new StringBuilder();
+                foreach (var col in cols)// cicla tutte le Entity della classe in oggetto
+                {
+                    line.Append(col.GetValue(row));
+                    line.Append(",");
+                }
+                lines.Add(line.ToString().Substring(0, line.Length - 1));
+            }
+
+            System.IO.File.WriteAllLines(filePath, lines);
+
         }
         public static List<T> LoadFromTextFileCsvHelper<T>(string filePath) where T : class, new()
         {
